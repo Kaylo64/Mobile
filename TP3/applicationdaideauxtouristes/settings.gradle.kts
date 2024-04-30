2cd7752384b8e0737ff7407174c71ec4ddaaebf5
Kaylo64 <jeansst2003@gmail.com>
2024-04-30 16:31:34 -0400

    Ajouter les deux projets du TP3

---
+++ TP3/applicationdaideauxtouristes/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Application d’aide aux touristes"
+include(":app")
+ 
