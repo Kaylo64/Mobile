2e0b01f0abaf13b2409814480a7eacf10d844f83
Kaylo64 <jeansst2003@gmail.com>
2024-04-30 19:39:53 -0400

    Ajouter logic pour TP3 Exercice1

--- TP3/applicationdaideauxtouristes/app/src/main/java/com/example/applicationdaideauxtouristes/DescriptionVille.java
+++ TP3/applicationdaideauxtouristes/app/src/main/java/com/example/applicationdaideauxtouristes/DescriptionVille.java
@@ -2,7 +2,11 @@ package com.example.applicationdaideauxtouristes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DescriptionVille extends AppCompatActivity {
 
@@ -10,5 +14,45 @@ public class DescriptionVille extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_ville);
+
+        Intent intent = getIntent();
+        Button mainPageBtn = findViewById(R.id.mainPageBtn);
+
+        if (intent != null) {
+            String ville = intent.getStringExtra("ville");
+            afficherDescription(ville);
+            afficherLienDeLaVille(ville);
+        }
+        mainPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retourPagePrincipale();
+            }
+        });
     }
+        private void afficherDescription(String ville) {
+            TextView descriptionTextView = findViewById(R.id.descriptionTextView);
+            String description = "";
+            if ("Montréal".equals(ville)) {
+                description = getString(R.string.description_montreal);
+            } else if ("Granby".equals(ville)) {
+                description = getString(R.string.description_granby);
+            }
+            descriptionTextView.setText(description);
+        }
+
+        private void afficherLienDeLaVille(String ville){
+            TextView urlTextView = findViewById(R.id.urlTextView);
+            String url = "";
+            if ("Montréal".equals(ville)) {
+                url = getString(R.string.site_web_montreal);
+            } else if ("Granby".equals(ville)) {
+                url = getString(R.string.site_web_granby);
+            }
+            urlTextView.setText(url);
+        }
+        private void retourPagePrincipale() {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
 }
--- TP3/applicationdaideauxtouristes/app/src/main/java/com/example/applicationdaideauxtouristes/MainActivity.java
+++ TP3/applicationdaideauxtouristes/app/src/main/java/com/example/applicationdaideauxtouristes/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.applicationdaideauxtouristes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mltBtn = findViewById(R.id.mltBtn);
+        Button granbyBtn = findViewById(R.id.granbyBtn);
+        mltBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirDescriptionVille("Montréal");
+            }
+        });
+        granbyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirDescriptionVille("Granby");
+            }
+        });
+    }
+
+    private void ouvrirDescriptionVille(String ville) {
+        Intent intent = new Intent(this, DescriptionVille.class);
+        intent.putExtra("ville", ville);
+        startActivity(intent);
     }
 }
