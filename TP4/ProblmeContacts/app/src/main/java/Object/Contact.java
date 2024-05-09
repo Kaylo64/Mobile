b0ca9b168b32e99c741794743a44bb98b630d0bf
Kaylo64 <jeansst2003@gmail.com>
2024-05-08 20:16:44 -0400

    Ajouter la classe Contact

---
+++ TP4/ProblmeContacts/app/src/main/java/Object/Contact.java
@@ -0,0 +1,39 @@
+package Object;
+
+public class Contact {
+    private String nom;
+    private String courriel;
+    private String telephone;
+
+    public Contact(String nom, String courriel, String telephone) {
+        this.nom = nom;
+        this.courriel = courriel;
+        this.telephone = telephone;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getCourriel() {
+        return courriel;
+    }
+    public void setCourriel(String courriel) {
+        this.courriel = courriel;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "nom='" + nom + '\'' +
+                ", courriel='" + courriel + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
